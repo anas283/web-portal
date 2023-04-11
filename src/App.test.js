1242af8dd002c6c8f37acc5a0370258d94cf43ca
Muhammad Anas <muhammadanasbinadam@gmail.com>
2023-04-12 07:20:30 +0800

    update: test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders login', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Welcome back, please enter your credentials/i);
   expect(linkElement).toBeInTheDocument();
 });
